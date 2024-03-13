53aa5ed027a9806a54f838015830415e668c6071
manish07 <manish.nmamit@gmail.com>
2024-03-13 22:54:26 +0530

    output

--- Program.cs
+++ Program.cs
@@ -10,3 +10,13 @@ LimitedObjectsCreation object4 = LimitedObjectsCreation.CreateObject();
 LimitedObjectsCreation object5 = LimitedObjectsCreation.CreateObject();
 
 LimitedObjectsCreation object6 = LimitedObjectsCreation.CreateObject();
+
+/*---Output----------------------------------------------------------------
+# Output
+1 Object created successfully
+2 Object created successfully
+3 Object created successfully
+4 Object created successfully
+5 Object created successfully
+Cannot create more object. Maximum limit 5 reached.
+*/
